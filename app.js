403c6ad4b361dde0804944f9ddbda5c274ffa0c8
Christina Annas <cannas@ipponusa.com>
2022-02-26 14:15:08 -0500

    Adding a new component to represent the list of offices

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 import { WeatherCardComponent } from "./components/weather-card.comp.js";
+import { IpponWeatherListComponent } from "./components/ippon-weather-list.comp.js"; 
 
 window.customElements.define('weather-card-component', WeatherCardComponent);
+window.customElements.define('ippon-weather-list-component', IpponWeatherListComponent);
