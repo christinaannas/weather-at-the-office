600876bbed3cf4cf66f015a40a4e5c1df296a4bd
Christina Annas <cannas@ipponusa.com>
2022-02-26 15:51:45 -0500

    Generating inner HTML programmatically.

--- components/ippon-weather-list.comp.js
+++ components/ippon-weather-list.comp.js
@@ -2,6 +2,41 @@ export class IpponWeatherListComponent extends HTMLElement {
 
   constructor() {
     var that = super();
+    that.offices = [
+      {},
+      {
+        temp: 0
+      },
+      {
+        location: "Richmond, Virginia",
+        temp: 36,
+        condition: "clear",
+        updated: "2022-01-31 20:30",
+        color_class: "pink"
+      },
+      {
+        location: "Washington, District of Columbia",
+        temp: 30,
+        condition: "partly cloudy",
+        updated: "2022-01-31 20:30",
+        color_class: "purple"
+      },
+      {
+        location: "New York, New York",
+        temp: 28,
+        condition: "partly cloudy",
+        updated: "2022-01-31 20:15",
+        color_class: "blue"
+      },
+      {
+        location: "Atlanta, Georgia",
+        temp: 43.7,
+        condition: "clear",
+        updated: "2022-01-31 20:15",
+        color_class: "green"
+      }
+    ];
+
     const shadowRoot = that.attachShadow({mode: 'open'});
 
     const divElement = document.createElement('div');
@@ -11,40 +46,17 @@ export class IpponWeatherListComponent extends HTMLElement {
   }
 
   getInnerHTML() {
-    return `
-<h1>Weather Conditions at Ippon Offices</h1>
-<weather-card-component></weather-card-component>
-<weather-card-component
-  temp=0
-></weather-card-component>
-<weather-card-component
-  location="Richmond, Virginia"
-  temp="36"
-  condition="clear"
-  updated="2022-01-31 20:30"
-  color_class="pink"
-></weather-card-component>
-<weather-card-component
-  location="Washington, District of Columbia"
-  temp="30"
-  condition="partly cloudy"
-  updated="2022-01-31 20:30"
-  color_class="purple"
-></weather-card-component>
-<weather-card-component
-  location="New York, New York"
-  temp="28"
-  condition="partly cloudy"
-  updated="2022-01-31 20:15"
-  color_class="blue"
-></weather-card-component>
-<weather-card-component
-  location="Atlanta, Georgia"
-  temp="43.7"
-  condition="clear"
-  updated="2022-01-31 20:15"
-  color_class="green"
-></weather-card-component>
-    `;
+    var that = this;
+    const header = "<h1>Weather Conditions at Ippon Offices</h1>";
+
+    function customElementFromOfficeObject(officeObject) {
+      const attributes = Object.entries(officeObject)
+          .map(([attributeName, attributeValue]) => `${attributeName}="${attributeValue}"`);
+          return `<weather-card-component ${attributes.join(" ")}></weather-card-component>`;
+    }
+
+    const officeCards = that.offices
+        .map((officeObject) => customElementFromOfficeObject(officeObject));
+    return header + officeCards.join(" ");
   }
 }
